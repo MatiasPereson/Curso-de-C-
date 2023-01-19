da549f04c7c25b9e577409850eac85222b23f725
Matias Pereson <matias.pereson@desarrollosnea.com>
2023-01-19 13:44:40 -0300

    primer commit bien hecho

---
+++ Introduccion-Ejercicio 1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingresa tu nombre y te dire como te llamas");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, tu nombre es " + nombre);
---
+++ Introduccion-Ejercicio 2/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+var fecha = DateTime.Now;
+Console.WriteLine("La fecha de hoy es: " + fecha.ToString("d"));
